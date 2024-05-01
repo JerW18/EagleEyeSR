7b57309e495230bd34f0f5b58cd871dffa33a0c2
Kenshin F <kenshin_fetalvero@dlsu.edu.ph>
2024-05-01 15:58:52 +0800

    Update gradle config

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,7 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 #opencv
-OPENCVROOT:= C:\Users\NeilDG\Documents\GithubProjects\opencv-3.3.0-android-sdk\OpenCV-android-sdk
+# Download OpenCV from https://sourceforge.net/projects/opencvlibrary/files/opencv-android/3.3.0/opencv-3.3.0-android-sdk.zip/download
+# Replace this with own path
+OPENCVROOT:= C:\Users\Kenshin\Desktop\OpenCV-android-sdk
 OPENCV_CAMERA_MODULES:=off
 OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
